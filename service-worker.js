41b27b85b4b2749426f1e2773bc2a14e90b73dba
dammiezen <126914752+dammiezen@users.noreply.github.com>
2026-07-14 05:54:25 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE="deba-calculators-v1";
+const CACHE="deba-calculators-v2";
 const FILES=["./","./index.html","./brrrr.html","./financed-flip.html","./cash-buy.html","./wholesale.html","./styles.css","./common.js","./calc-utils.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png","./icons/maskable-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
